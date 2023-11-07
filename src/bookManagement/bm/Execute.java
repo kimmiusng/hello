6ab53eabd50ba965edffeb6e55a51360550dc613
misung <mmmmme814@gamil.com>
2023-11-07 17:49:43 +0900

    도서관리프로그램 요구사항3 작성중!

--- src/bookManagement/bm/Execute.java
+++ src/bookManagement/bm/Execute.java
@@ -4,7 +4,7 @@ import bookManagement.bm.BM2;
 import bookManagement.bm.BookManager;
 
 public class Execute {
-    static BookManager bm = new BM2();
+    static BookManager bm = new BM3();
     public static void main(String[] args) {
 
         bm.init();
